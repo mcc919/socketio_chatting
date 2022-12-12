02c44a1737ea9f7f9e1047e3fa83c9f284024e70
Hotaek Han <ryanhan919@gmail.com>
2022-12-12 21:57:23 +0900

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const { emit } = require('process');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.send('<h1> Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('new user connected!');
+    io.emit('user_connect', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        io.emit('user_disconnect', socket.id);
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(3001, () => {
     console.log('listening on *:3000');
 });
